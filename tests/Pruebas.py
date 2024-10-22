86d9d931a2f4b0f1e821908235b77144ef12adea
labf402 <75244564@continental.edu.pe>
2024-10-22 16:41:02 -0500

    Se realizaron pruebas con la Media con dos elemento (Verde)

--- tests/Pruebas.py
+++ tests/Pruebas.py
@@ -15,6 +15,11 @@ class PruebasDesvEstandar(unittest.TestCase):
         self.DesviacionEstandar.agregar_elemento(5)
         self.assertEqual(self.DesviacionEstandar.media(), 5)
 
+    def test_media_dos_elementos(self):
+        self.DesviacionEstandar.agregar_elemento(4)
+        self.DesviacionEstandar.agregar_elemento(6)
+        self.assertEqual(self.DesviacionEstandar.media(), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
